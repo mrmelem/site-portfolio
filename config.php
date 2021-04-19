750513d92c86c68c3cd7e07004d33691bf13bb46
Pedro Melém <Melemredes@gmail.com>
2021-04-19 02:16:24 -0300

    Update

--- config.php
+++ config.php
@@ -4,14 +4,10 @@ session_start();
 
 function myAutoLoad($class)
 {
-    if ($class == "Email" && file_exists('./src/class/email.php' && file_exists('./src/class/phpmailer/vendor/autoload.php'))) {
-        try {
-            include('.src/class/phpmailer/vendor/autoload.php');
-            include('.src/class/email.php');
-        } catch (Exception $e) {
-            die("Classe Mailer não foi encontrada");
-        }
-    } else if (file_exists('./src/class/' . $class . '.php')) {
+    if ($class == "PHPMailer"){
+        include('./src/class/phpmailer/PHPMailerAutoload.php');
+    }
+    else if (file_exists('./src/class/' . $class . '.php')) {
         include('./src/class/' . $class . '.php');
     } else {
         die("Classe:" . $class . " não foi encontrada");
@@ -23,4 +19,6 @@ spl_autoload_register('myAutoLoad');
 define('INCLUDE_PATH', '/site-portfolio/');
 define('STYLES_PATH', INCLUDE_PATH . 'src/pages/');
 include("./bin/keys.php");
-new Email();
+
+
+
